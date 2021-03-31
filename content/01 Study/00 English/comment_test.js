6891b8086e3e075800324295295aefb814315468
samsamhan <dlwngud6333@gmail.com>
2021-03-31 21:39:38 +0900

    20210331 Diary

---
+++ content/01 Study/00 English/comment_test.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class SshComments extends React.Component {
+  render() {
+    return(
+      <div>
+        aaaaaaaaaaaaa
+        <script src="https://utteranc.es/client.js"
+                repo="samsamhan/ssh_blog_comments"
+                issue-term="pathname"
+                theme="github-light"
+                crossorigin="anonymous"
+                async>
+        </script>
+      </div>
+    )
+  }
+}
+
+export default SshComments
