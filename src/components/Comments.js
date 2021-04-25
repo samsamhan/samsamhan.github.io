80145af6fce202776b04b163b36f45a38bf2bd97
samsamhan <dlwngud6333@gmail.com>
2021-04-25 22:48:47 +0900

    Comments 중간 Upload

---
+++ src/components/Comments.js
@@ -0,0 +1,30 @@
+import React, { createRef, useEffect } from 'react'
+
+const Comments = () => {
+  const commentRef = createRef()
+
+  useEffect(() => {
+    const utterances = document.createElement('script')
+
+    const utterancesConfig = {
+      src:'https://utteranc.es/client.js'
+      , repo: 'samsamhan/ssh_blog_comments'
+      , theme: 'github-light'
+      , 'issue-term': 'pathname'
+      , async: true
+      , crossorigin: 'anonymous'
+    }
+
+    Object.entries(utterancesConfig).forEach(([key, value]) =>{
+      utterances.setAttribute(key,value)
+    })
+
+    commentRef.current.appendChild(utterances)
+
+  }, [])
+
+  return <div class="comments" ref={commentRef}></div>
+
+}
+
+export default Comments
